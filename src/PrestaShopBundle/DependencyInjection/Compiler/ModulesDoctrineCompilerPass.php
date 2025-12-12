6c8b45c26cd65a986447b7679a8ce8da41e1dbcb
Codencode <info@codencode.it>
2025-12-12 12:51:08 +0100

    Refactor: resolve module namespace from src/Entity directory

--- src/PrestaShopBundle/DependencyInjection/Compiler/ModulesDoctrineCompilerPass.php
+++ src/PrestaShopBundle/DependencyInjection/Compiler/ModulesDoctrineCompilerPass.php
@@ -27,7 +27,6 @@
 namespace PrestaShopBundle\DependencyInjection\Compiler;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
-use PrestaShop\PrestaShop\Core\Util\Inflector;
 use Symfony\Component\Config\Resource\DirectoryResource;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -75,12 +74,14 @@ class ModulesDoctrineCompilerPass implements CompilerPassInterface
             if (in_array($moduleFolder->getFilename(), $activeModules)
                 && is_dir($moduleFolder . '/src/Entity')
             ) {
-                $moduleNamespace = $this->getModuleNamespace($moduleFolder);
+                $moduleEntityDirectory = realpath($moduleFolder . '/src/Entity');
+                if ($moduleEntityDirectory === false) {
+                    continue;
+                }
+                $moduleNamespace = $this->getModuleNamespace($moduleEntityDirectory);
                 if (empty($moduleNamespace)) {
                     continue;
                 }
-                $modulePrefix = 'Module' . Inflector::getInflector()->camelize($moduleFolder->getFilename());
-                $moduleEntityDirectory = realpath($moduleFolder . '/src/Entity');
                 $mappingPass = $this->createAnnotationMappingDriver($moduleNamespace, $moduleEntityDirectory);
                 $mappingsPassList[$moduleEntityDirectory] = $mappingPass;
             }
@@ -113,14 +114,15 @@ class ModulesDoctrineCompilerPass implements CompilerPassInterface
     }
 
     /**
-     * @param SplFileInfo $moduleFolder
+     * @param string $moduleEntityDirectory
      *
      * @return string
      */
-    private function getModuleNamespace(SplFileInfo $moduleFolder)
+    private function getModuleNamespace(string $moduleEntityDirectory)
     {
         $finder = new Finder();
-        $finder->files()->in($moduleFolder)->name('*.php');
+        $finder->files()->in($moduleEntityDirectory)->name('*.php');
+
         foreach ($finder as $phpFile) {
             if (preg_match('~namespace[ \t]+(.*)[ \t]*;~Um', $phpFile->getContents(), $matches)) {
                 if (($namespace = trim($matches[1])) === '') {
