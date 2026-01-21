13283bec93393a713e3cf7f4d50ecd8240760b4c
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2026-01-21 08:54:36 +0100

    Update UI tests for API check endpoints

--- tests/UI/campaigns/functional/API/02_checkEndpoints.ts
+++ tests/UI/campaigns/functional/API/02_checkEndpoints.ts
@@ -88,11 +88,21 @@ describe('API : Check endpoints', async () => {
         // @todo: add tests
         '/addresses/bulk-delete: DELETE',
         // @todo: add tests
+        '/addresses/carts/{cartId}: PATCH',
+        // @todo: add tests
         '/addresses/customers/{addressId}: GET',
         // @todo: add tests
         '/addresses/customers/{addressId}: PATCH',
         // @todo: add tests
         '/addresses/customers: POST',
+        // @todo: add tests
+        '/addresses/manufacturers/{addressId}: GET',
+        // @todo: add tests
+        '/addresses/manufacturers/{addressId}: PATCH',
+        // @todo: add tests
+        '/addresses/manufacturers: POST',
+        // @todo: add tests
+        '/addresses/orders/{orderId}: PATCH',
         // @todo: add tests
         '/addresses/{addressId}: DELETE',
         // @todo: add tests
@@ -249,6 +259,8 @@ describe('API : Check endpoints', async () => {
         '/modules/{technicalName}: GET',
         // tests/UI/campaigns/functional/API/02_endpoints/07_module/11_getModules.ts
         '/modules: GET',
+        // @todo: add tests
+        '/products/combinations/{combinationId}: GET',
         // tests/UI/campaigns/functional/API/02_endpoints/08_product/01_deleteProductImageId.ts
         '/products/images/{imageId}: DELETE',
         // tests/UI/campaigns/functional/API/02_endpoints/08_product/02_getProductImageId.ts
@@ -257,6 +269,12 @@ describe('API : Check endpoints', async () => {
         '/products/images/{imageId}: POST',
         // tests/UI/campaigns/functional/API/02_endpoints/08_product/11_getProductsSearch.ts
         '/products/search: GET',
+        // @todo: add tests
+        '/products/{productId}/combination-ids: GET',
+        // @todo: add tests
+        '/products/{productId}/combinations: GET',
+        // @todo: add tests
+        '/products/{productId}/generate-combinations: POST',
         // tests/UI/campaigns/functional/API/02_endpoints/08_product/05_getProductIdImages.ts
         '/products/{productId}/images: GET',
         // tests/UI/campaigns/functional/API/02_endpoints/08_product/04_postProductIdImage.ts
