e126eb5cb520f26cde0c783d0570302be7a28257
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-04-09 15:32:06 +0200

    Complete the scenario

--- tests/UI/campaigns/functional/BO/03_catalog/07_discounts/02_catalogPriceRules/05_CRUDCountry.ts
+++ tests/UI/campaigns/functional/BO/03_catalog/07_discounts/02_catalogPriceRules/05_CRUDCountry.ts
@@ -1,37 +1,51 @@
 import testContext from '@utils/testContext';
 import {expect} from 'chai';
-import {createCurrencyTest, deleteCurrencyTest} from '@commonTests/BO/international/currency';
 
 import {
+  // Import BO pages
   boCartRulesPage,
   boCatalogPriceRulesCreatePage,
   boCatalogPriceRulesPage,
   boDashboardPage,
   boLocalizationPage,
   boLoginPage,
-  type BrowserContext,
-  dataCountries,
-  dataCurrencies,
-  dataCustomers,
-  dataGroups,
-  dataProducts,
-  FakerCatalogPriceRule,
+  // Import FO pages
   foHummingbirdCartPage,
-  foHummingbirdCategoryPage,
   foHummingbirdSearchResultsPage,
   foHummingbirdHomePage,
   foHummingbirdLoginPage,
   foHummingbirdProductPage,
+  foHummingbirdMyAccountPage,
+  foHummingbirdMyAddressesPage,
+  foHummingbirdMyAddressesCreatePage,
+  // Import data
+  dataProducts,
+  FakerCustomer,
+  FakerCatalogPriceRule,
+  FakerAddress,
+  // Import type
+  type BrowserContext,
   type ImportContent,
   type Page,
   utilsPlaywright,
 } from '@prestashop-core/ui-testing';
+
+import {createCustomerTest, deleteCustomerTest} from '@commonTests/BO/customers/customer';
 
 const baseContext: string = 'functional_BO_catalog_discounts_catalogPriceRules_CRUDCountry';
 
 describe('BO - Catalog - Discounts : CRUD country', async () => {
   let browserContext: BrowserContext;
   let page: Page;
+  const customerData: FakerCustomer = new FakerCustomer({email: 'test@test.com', password: 123456789});
+  const addressData: FakerAddress = new FakerAddress({
+    email: customerData.email,
+    country: 'United Arab Emirates',
+  });
+  const editAddressData: FakerAddress = new FakerAddress({
+    email: customerData.email,
+    country: 'United States',
+  });
 
   const contentToImport: ImportContent = {
     importStates: true,
@@ -62,6 +76,16 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
     fromQuantity: 1,
     reduction: 10.00,
   });
+  const editCatalogPriceRuleData2: FakerCatalogPriceRule = new FakerCatalogPriceRule({
+    name: 'test',
+    currency: 'All currencies',
+    country: 'All countries',
+    group: 'All groups',
+    reductionType: 'Amount',
+    reductionTax: 'Tax included',
+    fromQuantity: 1,
+    reduction: 10.00,
+  });
 
   before(async function () {
     browserContext = await utilsPlaywright.createBrowserContext(this.browser);
@@ -72,17 +96,20 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
     await utilsPlaywright.closeBrowserContext(browserContext);
   });
 
-  it('should login in BO', async function () {
-    await testContext.addContextItem(this, 'testIdentifier', 'loginBO', baseContext);
-
-    await boLoginPage.goTo(page, global.BO.URL);
-    await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
-
-    const pageTitle = await boDashboardPage.getPageTitle(page);
-    expect(pageTitle).to.contains(boDashboardPage.pageTitle);
-  });
+  // Pre-condition: Create customer
+  createCustomerTest(customerData, `${baseContext}_preTest`);
 
   describe('Import localization pack of United states', async () => {
+    it('should login in BO', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'loginBO', baseContext);
+
+      await boLoginPage.goTo(page, global.BO.URL);
+      await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
+
+      const pageTitle = await boDashboardPage.getPageTitle(page);
+      expect(pageTitle).to.contains(boDashboardPage.pageTitle);
+    });
+
     it('should go to \'International > Localization\' page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'goToLocalizationPage', baseContext);
 
@@ -114,7 +141,7 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
     });
   });
 
-  describe('Create catalog price rule', async () => {
+  describe('Create catalog price rule in Bo and check it in FO', async () => {
     it('should go to \'Catalog > Discounts\' page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'goToDiscountsPage', baseContext);
 
@@ -146,15 +173,13 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
       expect(pageTitle).to.contains(boCatalogPriceRulesCreatePage.pageTitle);
     });
 
-    it('should create new catalog price rule', async function () {
+    it('should create new catalog price rule with country : All', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'createCatalogPriceRule', baseContext);
 
       const validationMessage = await boCatalogPriceRulesCreatePage.setCatalogPriceRule(page, catalogPriceRuleData);
       expect(validationMessage).to.contains(boCatalogPriceRulesPage.successfulCreationMessage);
     });
-  });
 
-  describe('Check catalog price rule in FO', async () => {
     it('should view my shop', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'viewMyShop_1', baseContext);
 
@@ -175,7 +200,7 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
       expect(pageTitle).to.equal(foHummingbirdSearchResultsPage.pageTitle);
     });
 
-    it('should go to the product page', async function(){
+    it('should go to the product page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'goToProductPage', baseContext);
 
       await foHummingbirdSearchResultsPage.goToProductPage(page, 1);
@@ -216,12 +241,16 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
     });
 
     it('should add the product to the cart', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkCart_1', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
 
       await foHummingbirdProductPage.addProductToTheCart(page);
 
       const pageTitle = await foHummingbirdCartPage.getPageTitle(page);
       expect(pageTitle).to.equal(foHummingbirdCartPage.pageTitle);
+    });
+
+    it('should check the discount in cart page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountInTheCart', baseContext);
 
       const productDetail = await foHummingbirdCartPage.getProductDetail(page, 1);
       await Promise.all([
@@ -236,7 +265,7 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
     });
   });
 
-  describe('Edit catalog price rules and check it in FO - Country', async ()=>{
+  describe('Edit catalog price rules and check it in FO - Country : United Arab Emirates', async () => {
     it('should go back to BO', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'goBackToBO', baseContext);
 
@@ -246,7 +275,7 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
       expect(pageTitle).to.contains(boCatalogPriceRulesPage.pageTitle);
     });
 
-    it('should edit the catalog price rules', async function(){
+    it('should edit the catalog price rules', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'editCatalogPriceRules', baseContext);
 
       await boCatalogPriceRulesPage.goToEditCatalogPriceRulePage(page, catalogPriceRuleData.name);
@@ -267,7 +296,7 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
       expect(pageTitle).to.contains(foHummingbirdCartPage.pageTitle);
     });
 
-    it('should check that no reduction is displayed', async function () {
+    it('should check that no reduction is applied', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'checkNoReduction', baseContext);
 
       await foHummingbirdCartPage.reloadPage(page);
@@ -284,7 +313,188 @@ describe('BO - Catalog - Discounts : CRUD country', async () => {
     });
   });
 
-  describe('Edit customer country', async function(){
+  describe('Add new customer Address - Country : United Arab Emirates', async () => {
+    it('should login with the created customer', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'loginCustomer', baseContext);
 
+      await foHummingbirdHomePage.goToLoginPage(page);
+      await foHummingbirdLoginPage.customerLogin(page, customerData);
+
+      const connected = await foHummingbirdHomePage.isCustomerConnected(page);
+      expect(connected, 'Customer is not connected in FO').to.eq(true);
+    });
+
+    it('should go to \'My Account\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToMyAccountPage', baseContext);
+
+      await foHummingbirdHomePage.goToMyAccountPage(page);
+
+      const pageHeaderTitle = await foHummingbirdMyAccountPage.getPageTitle(page);
+      expect(pageHeaderTitle).to.equal(foHummingbirdMyAccountPage.pageTitle);
+    });
+
+    it('should go to \'Add first address\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToAddFirstAddressPage', baseContext);
+
+      await foHummingbirdMyAccountPage.goToAddressesPage(page);
+
+      const pageHeaderTitle = await foHummingbirdMyAddressesPage.getPageTitle(page);
+      expect(pageHeaderTitle).to.equal(foHummingbirdMyAddressesPage.addressPageTitle);
+    });
+
+    it('should create new address', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'createAddress', baseContext);
+
+      const textResult = await foHummingbirdMyAddressesCreatePage.setAddress(page, addressData);
+      expect(textResult).to.equal(foHummingbirdMyAddressesPage.addAddressSuccessfulMessage);
+    });
   });
+
+  describe('Check that the discount is applied in the cart page', async () => {
+    it('should go to the cart page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCart', baseContext);
+      await foHummingbirdMyAddressesCreatePage.goToCartPage(page);
+
+      const pageTitle = await foHummingbirdCartPage.getPageTitle(page);
+      expect(pageTitle).to.equal(foHummingbirdCartPage.pageTitle);
+    });
+
+    // @todo : https://github.com/PrestaShop/PrestaShop/issues/41241
+    it.skip('should check the discount', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountInTheCart_2', baseContext);
+
+      const productDetail = await foHummingbirdCartPage.getProductDetail(page, 1);
+      await Promise.all([
+        expect(productDetail.regularPrice).to.equal(dataProducts.demo_6.combinations[0].price),
+        expect(productDetail.price.toFixed(2)).to.equal(
+          (
+            dataProducts.demo_6.combinations[0].price - catalogPriceRuleData.reduction
+          ).toFixed(2),
+        ),
+        expect(productDetail.discountAmount).to.equal(`-€${catalogPriceRuleData.reduction.toFixed(2)}`),
+      ]);
+    });
+  });
+
+  describe('Edit catalog price rules and check it in FO - Country : All', async () => {
+    it('should go back to BO', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goBackToBO_2', baseContext);
+
+      page = await foHummingbirdCartPage.changePage(browserContext, 0);
+
+      const pageTitle = await boCatalogPriceRulesPage.getPageTitle(page);
+      expect(pageTitle).to.contains(boCatalogPriceRulesPage.pageTitle);
+    });
+
+    it('should edit the catalog price rules', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'editCatalogPriceRules_2', baseContext);
+
+      await boCatalogPriceRulesPage.goToEditCatalogPriceRulePage(page, editCatalogPriceRuleData.name);
+
+      const pageTitle = await boCatalogPriceRulesCreatePage.getPageTitle(page);
+      expect(pageTitle).to.contains(boCatalogPriceRulesCreatePage.editPageTitle);
+
+      const validationMessage = await boCatalogPriceRulesCreatePage.setCatalogPriceRule(page, editCatalogPriceRuleData2);
+      expect(validationMessage).to.contains(boCatalogPriceRulesPage.successfulUpdateMessage);
+    });
+
+    it('should go back to FO', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goBackToFO_2', baseContext);
+
+      page = await boCatalogPriceRulesCreatePage.changePage(browserContext, 1);
+
+      const pageTitle = await foHummingbirdCartPage.getPageTitle(page);
+      expect(pageTitle).to.contains(foHummingbirdCartPage.pageTitle);
+    });
+
+    // @todo : https://github.com/PrestaShop/PrestaShop/issues/41241
+    it.skip('should check the discount', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountInTheCart_3', baseContext);
+
+      const productDetail = await foHummingbirdCartPage.getProductDetail(page, 1);
+      await Promise.all([
+        expect(productDetail.regularPrice).to.equal(dataProducts.demo_6.combinations[0].price),
+        expect(productDetail.price.toFixed(2)).to.equal(
+          (
+            dataProducts.demo_6.combinations[0].price - catalogPriceRuleData.reduction
+          ).toFixed(2),
+        ),
+        expect(productDetail.discountAmount).to.equal(`-€${catalogPriceRuleData.reduction.toFixed(2)}`),
+      ]);
+    });
+  });
+
+  describe('Edit customer Address - Country : United States', async () => {
+    it('should go to \'My Account\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToMyAccountPage_2', baseContext);
+
+      await foHummingbirdHomePage.goToMyAccountPage(page);
+
+      const pageHeaderTitle = await foHummingbirdMyAccountPage.getPageTitle(page);
+      expect(pageHeaderTitle).to.equal(foHummingbirdMyAccountPage.pageTitle);
+    });
+
+    it('should go to addresses page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToAddressesPageToDeleteAddress', baseContext);
+
+      await foHummingbirdMyAccountPage.goToAddressesPage(page);
+
+      const pageHeaderTitle = await foHummingbirdMyAddressesPage.getPageTitle(page);
+      expect(pageHeaderTitle).to.equal(foHummingbirdMyAddressesPage.pageTitle);
+    });
+
+    it('should update the address', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'updateAddress', baseContext);
+
+      await foHummingbirdMyAddressesPage.goToEditAddressPage(page, 1);
+
+      const textResult = await foHummingbirdMyAddressesCreatePage.setAddress(page, editAddressData);
+      expect(textResult).to.equal(foHummingbirdMyAddressesPage.updateAddressSuccessfulMessage);
+    });
+
+    it('should go to the cart page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCart_2', baseContext);
+      await foHummingbirdMyAddressesCreatePage.goToCartPage(page);
+
+      const pageTitle = await foHummingbirdCartPage.getPageTitle(page);
+      expect(pageTitle).to.equal(foHummingbirdCartPage.pageTitle);
+    });
+
+    // @todo : https://github.com/PrestaShop/PrestaShop/issues/41241
+    it.skip('should check the discount', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDiscountInTheCart_3', baseContext);
+
+      const productDetail = await foHummingbirdCartPage.getProductDetail(page, 1);
+      await Promise.all([
+        expect(productDetail.regularPrice).to.equal(dataProducts.demo_6.combinations[0].price),
+        expect(productDetail.price.toFixed(2)).to.equal(
+          (
+            dataProducts.demo_6.combinations[0].price - catalogPriceRuleData.reduction
+          ).toFixed(2),
+        ),
+        expect(productDetail.discountAmount).to.equal(`-€${catalogPriceRuleData.reduction.toFixed(2)}`),
+      ]);
+    });
+  });
+
+  describe('Delete created catalog price rules', async () => {
+    it('should go back to BO', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goBackToBO_3', baseContext);
+
+      page = await foHummingbirdCartPage.changePage(browserContext, 0);
+
+      const pageTitle = await boCatalogPriceRulesPage.getPageTitle(page);
+      expect(pageTitle).to.contains(boCatalogPriceRulesPage.pageTitle);
+    });
+
+    it('should delete catalog price rule', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'deleteCatalogPriceRule', baseContext);
+
+      const deleteTextResult = await boCatalogPriceRulesPage.deleteCatalogPriceRule(page, editCatalogPriceRuleData2.name);
+      expect(deleteTextResult).to.contains(boCatalogPriceRulesPage.successfulDeleteMessage);
+    });
+  });
+
+  // Post-condition: Delete customer
+  deleteCustomerTest(customerData, `${baseContext}_postTest`);
 });
