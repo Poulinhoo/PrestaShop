433829c6790fd0e177257d2945792daaa857e742
Aurelien Rita <aurelien.rita@prestashop.com>
2026-06-15 16:14:15 +0200

    Lint fixer used and put Pasword as expected

--- tests/UI/campaigns/functional/BO/16_wallOfFame/05_howToContribute.ts
+++ tests/UI/campaigns/functional/BO/16_wallOfFame/05_howToContribute.ts
@@ -32,43 +32,40 @@ describe('BO - Wall of Fame : How to contribute', async () => {
     await testContext.addContextItem(this, 'testIdentifier', 'loginBO', baseContext);
 
     await boLoginPage.goTo(page, global.BO.URL);
-    await boLoginPage.successLogin(page, global.BO.EMAIL, 'prestashopdemo');
+    await boLoginPage.successLogin(page, global.BO.EMAIL, global.BO.PASSWD);
 
     const pageTitle = await boDashboardPage.getPageTitle(page);
     expect(pageTitle).to.contains(boDashboardPage.pageTitle);
   });
-  it('should go to \'Wall of Fame\' page', async function(){
+  it('should go to \'Wall of Fame\' page', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'goToWallOfFamePage', baseContext);
-    
+
     await boWallOfFamePage.goToSubMenu(page, '', boWallOfFamePage.wallOfFameLink);
 
     const pageTitle = await boWallOfFamePage.getPageTitle(page);
     expect(pageTitle).to.contains(boWallOfFamePage.pageTitle);
-  })
-  it('should check that the \'How to contribute\' section is visible', async function(){
+  });
+  it('should check that the \'How to contribute\' section is visible', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'checkHowToContributeSection', baseContext);
-    
-    const isHowToContributeVisible = await boWallOfFamePage.isHowToContributeVisible(page);
-    expect(isHowToContributeVisible, 'The \'How to contribute\' section is visible').to.be.true;
-  })
 
-  it('should check that the redirection of \'Contribute\' button works', async function(){
+    const isHowToContributeVisible = await boWallOfFamePage.isHowToContributeVisible(page);
+    expect(isHowToContributeVisible, 'The \'How to contribute\' section is visible').to.equal(true);
+  });
+
+  it('should check that the redirection of \'Contribute\' button works', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'checkContributeButtonRedirection', baseContext);
-    
-    
+
     const contributionPage = await boWallOfFamePage.clickContributeLink(page);
     const contributionPageTitle = await contributionPage.title();
     expect(contributionPageTitle).to.contains(boWallOfFamePage.contributePageTitle);
     await contributionPage.close();
-    
-  })
-  it('should check that the redirection of \'Join Slack\' button works', async function(){
+  });
+  it('should check that the redirection of \'Join Slack\' button works', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'checkJoinSlackButtonRedirection', baseContext);
-    
+
     const joinSlackPage = await boWallOfFamePage.clickJoinSlackLink(page);
     const joinSlackPageTitle = await joinSlackPage.title();
     expect(joinSlackPageTitle).to.contains(boWallOfFamePage.joinSlackPageTitle);
     await joinSlackPage.close();
-  })
-  
+  });
 });
