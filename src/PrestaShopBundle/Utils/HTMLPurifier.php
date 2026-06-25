da2f89c3cbb62f59bad7f497fb1ff7782b3ad038
PrestaShop <dev-smb-edition@prestashop.com>
2026-06-25 14:55:33 +0200

    Fix: Update cache directory handling in HTMLPurifier constructor

--- src/PrestaShopBundle/Utils/HTMLPurifier.php
+++ src/PrestaShopBundle/Utils/HTMLPurifier.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * For the full copyright and license information, please view the
  * docs/licenses/LICENSE.txt file that was distributed with this source code.
@@ -21,7 +22,8 @@ class HTMLPurifier
 
     public function __construct(
         private readonly Filesystem $filesystem,
-        #[Autowire(param: 'prestashop.legacy_cache_dir')]
+        // kernel.cache_dir (var/cache/dev/admin) is excluded by LegacyCacheClearer; using it avoids a race condition where var/cache/dev/purifier is deleted between __construct and purify().
+        #[Autowire(param: 'kernel.cache_dir')]
         private readonly string $cacheDir,
     ) {
         $config = HTMLPurifier_Config::createDefault();
@@ -29,7 +31,7 @@ class HTMLPurifier
         $config->set('Attr.EnableID', true);
         $config->set('Attr.AllowedFrameTargets', ['_blank']);
 
-        $serializerPath = $this->cacheDir . 'purifier';
+        $serializerPath = $this->cacheDir . DIRECTORY_SEPARATOR . 'purifier';
         $this->filesystem->mkdir($serializerPath);
 
         $config->set('Cache.SerializerPath', $serializerPath);
